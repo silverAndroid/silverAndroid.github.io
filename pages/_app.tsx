63edabe7b2032ef94c5b61808cb335b4b834d049
Rushil Perera <pererarushil1940@gmail.com>
2021-04-05 15:21:28 -0400

    feat: changed main font to Open Sans

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,16 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <Component {...pageProps} />
+      <main>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
