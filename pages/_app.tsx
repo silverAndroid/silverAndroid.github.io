919a27917b1e1656e464218de00b11cdb8ce935e
Rushil Perera <pererarushil1940@gmail.com>
2021-04-05 13:20:07 -0400

    feat: initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles['page-container']}>
+      <h1>Hi, I'm Rushil!</h1>
+    </div>
+  );
+}
