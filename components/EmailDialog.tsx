e7bacd3617cc992e625cf52358b72e19020b6bef
Rushil Perera <pererarushil1940@gmail.com>
2021-04-06 06:29:20 -0400

    feat: replaced button with icon button for closing dialog

--- components/EmailDialog.tsx
+++ components/EmailDialog.tsx
@@ -1,5 +1,9 @@
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Dialog } from "@reach/dialog";
+
 import "@reach/dialog/styles.css";
+import styles from "../styles/EmailDialog.module.css";
 
 export interface EmailDialogProps {
   onClose: () => void;
@@ -9,13 +13,25 @@ export interface EmailDialogProps {
 export function EmailDialog({ isOpen, onClose }: EmailDialogProps) {
   return (
     <Dialog isOpen={isOpen} onDismiss={onClose} aria-labelledby="email-content">
+      <FontAwesomeIcon
+        className={styles.closeIcon}
+        icon={faTimes}
+        role="button"
+        aria-label="Close dialog containing email address"
+        tabIndex={0}
+        onClick={onClose}
+        onKeyDown={(e) => {
+          if (e.code === "Enter" || e.code === "Space") {
+            onClose();
+          }
+        }}
+      />
       <p id="email-content">
         My email is{" "}
         <a href="mailto:rushil.perera1081@gmail.com">
           rushil.perera1081@gmail.com
         </a>
       </p>
-      <button onClick={onClose}>Close</button>
     </Dialog>
   );
 }
