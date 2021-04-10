c9e44ae3d1e5116043642b43ece1ea14f5e1c144
Rushil Perera <pererarushil1940@gmail.com>
2021-04-10 18:24:44 -0400

    feat: initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<main>
+	<h1>Hi, I'm Rushil!</h1>
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+	}
+</style>
