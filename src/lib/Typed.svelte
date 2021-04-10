01388bdbb175111483fef141a37b802b8ac02aed
Rushil Perera <pererarushil1940@gmail.com>
2021-04-10 18:44:37 -0400

    feat: added typed.js for 'about me' section

---
+++ src/lib/Typed.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Typed from 'typed.js';
+	import type { TypedOptions } from 'typed.js';
+	import { onMount } from 'svelte';
+
+	export let options: TypedOptions;
+	export let ariaLabel: string;
+
+	let typedElem;
+	let typed: Typed;
+
+	onMount(() => {
+		typed = new Typed(typedElem, options);
+
+		return () => {
+			typed.destroy();
+		};
+	});
+</script>
+
+<span bind:this={typedElem} aria-label={ariaLabel} />
+
+<style lang="scss">
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	import TypedComponent from '$lib/Typed.svelte';
+	import type { TypedOptions } from 'typed.js';
+
+	const strings = [
+		'making web apps using Angular and React.js',
+		'playing volleyball and badminton',
+		'writing backends using Node.js',
+		'watching anime',
+		'developing mobile apps (native or cross-platform)',
+		'trying new technologies like React Native and Nest.js'
+	];
+
+	const typedAriaLabel =
+		'I love ' +
+		strings.map((str, i) => `${i === strings.length - 1 ? 'and ' : ''}${str}`).join(', ');
+
+	const typedStrings = strings.map((str) => 'I love ' + str);
+
+	const typedOptions: TypedOptions = {
+		strings: typedStrings,
+		typeSpeed: 30,
+		loop: true,
+		backSpeed: 50,
+		backDelay: 5000
+	};
 </script>
 
 <main>
 	<h1>Hi, I'm Rushil!</h1>
+
+	<p class="typed-wrapper" aria-label={typedAriaLabel}>
+		<TypedComponent options={typedOptions} />
+	</p>
 </main>
 
 <style lang="scss">
@@ -13,5 +41,9 @@
 		justify-content: center;
 		align-items: center;
 		height: 100%;
+	}
+
+	.typed-wrapper {
+		margin-top: 0;
 	}
 </style>
